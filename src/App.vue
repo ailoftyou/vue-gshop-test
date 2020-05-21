41943456bc378bd8557d15c5704ccc34d13b2d18
ailoftyou <1465637069@qq.com>
2020-05-21 14:40:06 +0800

    vue gshop test 001

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <router-view />
+    <FootGuider v-show="$route.meta.showFooter"></FootGuider>
+  </div>
+</template>
+
+<script>
+
+import FootGuider from './components/footGuider/FootGuider'
+
+export default {
+  components : {
+    FootGuider
+  }
+}
+</script>
+
+<style lang="stylus" rel="styleshet/stylus">
+  #app
+    width 100%
+    height 100%
+    background #f5f5f5       
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,51 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+
+import Msite from '../pages/Misite/Msite.vue'
+import Search from '../pages/Search/Search.vue'
+import Order from '../pages/Order/Order.vue'
+import Profile from '../pages/Profile/Profile.vue'
+import Login from '../pages/Login/login.vue'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path : '/',
+      redirect : '/msite'
+    },
+    {
+      path : '/msite',
+      component : Msite,
+      meta: {
+        showFooter: true
+      }
+    },
+    {
+      path: '/search',
+      component: Search,
+      meta: {
+        showFooter: true
+      }
+    },
+    {
+      path: '/order',
+      component: Order,
+      meta: {
+        showFooter: true
+      }
+    },
+    {
+      path: '/profile',
+      component: Profile,
+      meta: {
+        showFooter: true
+      }
+    },
+    {
+      path: '/login',
+      component: Login
+    }
+  ]
+})
